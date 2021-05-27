a14f16eb8001e31b7d1e28168c526d9db0962f51
Víctor Manuel Velázquez Fuentes <victor.velazfuentez@gmail.com>
2021-05-27 09:38:57 -0500

    project

---
+++ scripts4/auth.js
@@ -0,0 +1,190 @@
+auth.onAuthStateChanged(user => {
+    console.log(user);
+    if (user) {
+        db.collection('friends').onSnapshot(snapshot => {
+            getFriends(snapshot.docs);
+        });
+        confMenu(user);
+    }
+    else {
+        confMenu();
+        getFriends([]);
+    }
+});
+
+
+const salir = document.getElementById('salir');
+salir.addEventListener('click', (e) => {
+    e.preventDefault();
+    auth.signOut().then(() => {
+        alert("El usuario ha salido del sistema");
+        console.log("El usuario salió");
+    }).catch(err => {
+        console.log(err);
+    });
+});
+
+function mensajeError(codigo) {
+    let mensaje = '';
+    switch (codigo) {
+        case 'auth/wrong-password':
+            mensaje = 'Su contraseña no es correcta';
+            break;
+        case 'auth/user-not-found':
+            mensaje = 'El usuario no existe o el correo no esta registrado';
+            break;
+        case 'auth/weak-password':
+            mensaje = 'Contraseña débil debe tener al menos 6 caracteres';
+            break;
+        default:
+            mensaje = 'Ocurrió un error al ingresar con este usuario ' + codigo;
+    }
+    return mensaje;
+}
+
+var welcome = ''
+const formaingresar = document.getElementById('formLogIn');
+formaingresar.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let correo = formaingresar['correo'].value;
+    let contrasena = formaingresar['contraseña'].value;
+
+    auth.signInWithEmailAndPassword(correo, contrasena).then(cred => {
+        let cerrar = document.getElementById('cerrar1');
+        cerrar.click();
+        formaingresar.reset();
+        formaingresar.querySelector('.error').innerHTML = '';
+    }).catch(err => {
+        formaingresar.querySelector('.error').innerHTML = mensajeError(err.code);
+        console.log(err);
+    });
+
+});
+
+const formaRegistrarse = document.getElementById('formSignIn');
+formaRegistrarse.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log("Entró")
+    const correo = formaRegistrarse['rcorreo'].value;
+    const contraseña = formaRegistrarse['rcontraseña'].value;
+    auth.createUserWithEmailAndPassword(correo, contraseña).then(cred => {
+        //Hasta aqui estaría el usuario creado
+        console.log("Se creo el usuario");
+        console.log(formaRegistrarse['rnombre'].value);
+        console.log(formaRegistrarse['rtelefono'].value);
+        console.log(formaRegistrarse['rdireccion'].value);
+        return db.collection('friends').doc(cred.user.uid).set({
+            nombre: formaRegistrarse['rnombre'].value,
+            telefono: formaRegistrarse['rtelefono'].value,
+            direccion: formaRegistrarse['rdireccion'].value
+        });
+    }).then(() => {
+        let cerrar2 = document.getElementById('cerrar2');
+        cerrar2.click();
+        formaRegistrarse.reset();
+        formaRegistrarse.querySelector('.error').innerHTML = '';
+    }).catch(err => {
+        formaRegistrarse.querySelector('.error').innerHTML = mensajeError(err.code);
+    })
+})
+
+logInGoogle = () => {
+    var provider = new firebase.auth.GoogleAuthProvider();
+
+    firebase.auth().signInWithPopup(provider).then((result) => {
+        var token = result.credential.accessToken;
+        console.log(token);
+        var user = result.user;
+
+        let html = `
+        <p>Nombre: ${user.displayName}</p>
+        <p>Correo: ${user.email}</p>
+        <div class="d-flex justify-content-center">
+            <img style="width: 200px; height: 200px; border-radius: 50%;" src="${user.photoURL}"/>
+        </div>
+        `;
+
+
+        misDatos.innerHTML = html;
+        let cerrar = document.getElementById('cerrar1');
+        cerrar.click();
+        formaingresar.reset();
+        formaingresar.querySelector('.error').innerHTML = '';
+    }).catch((err) => {
+        console.log(err)
+    })
+}
+
+logInGithub = () => {
+    var provider = new firebase.auth.GithubAuthProvider();
+    firebase.auth().signInWithPopup(provider).then((result) => {
+        var token = result.credential.accessToken;
+        console.log(token);
+        var user = result.user;
+
+        let html = `
+        <p>Nombre: ${user.displayName}</p>
+        <p>Correo: ${user.email}</p>
+        <div class="d-flex justify-content-center">
+            <img style="width: 200px; height: 200px; border-radius: 50%;" src="${user.photoURL}"/>
+        </div>
+        `;
+        misDatos.innerHTML = html;
+        let cerrar = document.getElementById('cerrar1');
+        cerrar.click();
+        formaingresar.reset();
+        formaingresar.querySelector('.error').innerHTML = '';
+    }).catch((err) => {
+        console.log(err)
+    })
+}
+
+logInApple = () => {
+    var provider = new firebase.auth.OAuthProvider('apple.com');
+    firebase.auth().signInWithPopup(provider).then((result) => {
+        var token = result.credential.accessToken;
+        console.log(token);
+        var user = result.user;
+
+        let html = `
+        <p>Nombre: ${user.displayName}</p>
+        <p>Correo: ${user.email}</p>
+        <div class="d-flex justify-content-center">
+            <img style="width: 200px; height: 200px; border-radius: 50%;" src="${user.photoURL}"/>
+        </div>
+        `;
+
+
+        misDatos.innerHTML = html;
+        let cerrar = document.getElementById('cerrar1');
+        cerrar.click();
+        formaingresar.reset();
+        formaingresar.querySelector('.error').innerHTML = '';
+    }).catch((err) => {
+        console.log(err)
+    })
+}
+
+logInFacebook = () => {
+    var provider = new firebase.auth.FacebookAuthProvider();
+    firebase.auth().signInWithPopup(provider).then((result) => {
+        var token = result.credential.accessToken;
+        console.log(token);
+        var user = result.user;
+        let html = `
+        <p>Nombre: ${user.displayName}</p>
+        <p>Correo: ${user.email}</p>
+        <div class="d-flex justify-content-center">
+            <img style="width: 200px; height: 200px; border-radius: 50%;" src="${user.photoURL}"/>
+        </div>
+        `;
+        misDatos.innerHTML = html;
+        let cerrar = document.getElementById('cerrar1');
+        cerrar.click();
+        formaingresar.reset();
+        formaingresar.querySelector('.error').innerHTML = '';
+    }).catch((err) => {
+        console.log(err)
+    })
+}
+
---
+++ scripts4/index.js
@@ -0,0 +1,64 @@
+const listanonlogged = document.querySelectorAll('.non-logged');
+const listalogged = document.querySelectorAll('.logged');
+const misDatos = document.querySelector('.datosdelacuenta');
+
+//Cada que se loguea un usuario o salio
+const confMenu = (user) => {
+    if (user) {
+        db.collection('friends').doc(user.uid).get().then(doc => {
+            console.log(doc);
+            const html = `
+            <p>Nombre: ${doc.data().nombre}</p>
+            <p>Correo: ${user.email}</p>
+            <p>Teleono>: ${doc.data().telefono}</p>
+            <p>Direccion: ${doc.data().direccion}</p>
+            `;
+            misDatos.innerHTML = html;
+        });
+        listalogged.forEach(item => {
+            item.style.display = 'block';
+        });
+        listanonlogged.forEach(item => {
+            item.style.display = 'none';
+        });
+    } else {
+        listalogged.forEach(item => item.style.display = 'none')
+        listanonlogged.forEach(item => item.style.display = 'block')
+    }
+}
+
+
+
+const dishesList = document.getElementById('dishesList');
+const getFriends = (data) => {
+    if (data.length) {
+        let html = '';
+
+        data.forEach(doc => {
+            const platillo = doc.data();
+            const column = `
+            <div class="col-12 col-md-4">
+                <img src="${platillo.image}"
+                    alt="platillo1">
+                <div class="row m-2 d-flex justify-content-between">
+                    <p>
+                    ${platillo.name}</p>
+                    <p class="text-danger">$${platillo.price}</p>
+                </div>
+
+                <a href="https://www.paypal.me/grupohernandezalba/${platillo.price}" target="_blank">
+                    <button class="btn btn-primary">Pagar ahora</button>
+                </a>
+            </div>
+            `;
+
+            html += column;
+
+        });
+
+        //dishesList.innerHTML = html;
+    }
+    else {
+        //dishesList.innerHTML = '<div style="text-align: center; width: 100%; padding-top: 2rem"><p> Por favor inicie sesión en nuestra aplicación para ver el menú</p></div>'
+    }
+}
